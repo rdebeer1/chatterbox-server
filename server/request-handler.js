00dcd9f55041a13fdbd46b9fd3f234db99868575
rdebeer1 <Robert.DeBeer@gmail.com>
2018-02-05 12:31:21 -0600

    Imports handle method into basic server

--- server/request-handler.js
+++ server/request-handler.js
@@ -31,6 +31,21 @@ var requestHandler = function(request, response) {
 
   // The outgoing status.
   var statusCode = 200;
+  // These headers will allow Cross-Origin Resource Sharing (CORS).
+  // This code allows this server to talk to websites that
+  // are on different domains, for instance, your chat client.
+  //
+  // Your chat client is running from a url like file://your/chat/client/index.html,
+  // which is considered a different domain.
+  //
+  // Another way to get around this restriction is to serve you chat
+  // client from this domain by setting up static file serving.
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -55,19 +70,7 @@ var requestHandler = function(request, response) {
   response.end('Hello, World!');
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+
+exports.requestHandler = requestHandler;
 
